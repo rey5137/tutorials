694bf67c4664c620da9b27deafe8b5489e491a12
rey5137 <pea5137@gmail,com>
2015-05-24 23:15:07 +0700

    Initial commit

---
+++ app/src/main/java/com/rey/tutorial/MainActivity.java
@@ -0,0 +1,17 @@
+package com.rey.tutorial;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Rey on 5/24/2015.
+ */
+public class MainActivity extends FragmentActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
